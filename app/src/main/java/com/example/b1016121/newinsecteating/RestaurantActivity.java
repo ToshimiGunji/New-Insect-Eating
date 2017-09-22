36ab00e2ca99567f4f10010f3587228fdfbe8a0a
Keita <bonjovi1926@yahoo.co.jp>
2017-09-23 07:31:54 +0900

    restaurant layout

--- app/src/main/java/com/example/b1016121/newinsecteating/RestaurantActivity.java
+++ app/src/main/java/com/example/b1016121/newinsecteating/RestaurantActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.b1016121.newinsecteating.model.Restaurant;
 
@@ -17,10 +18,19 @@ public class RestaurantActivity extends AppCompatActivity {
         return intent.putExtra("restaurant",restaurant);
     }
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+
+        TextView nameView = (TextView)findViewById(R.id.name);
+        TextView descriptionView = (TextView)findViewById(R.id.description);
+
+        Restaurant restaurant = getIntent().getParcelableExtra("restaurant");
+        nameView.setText(restaurant.getName());
+        descriptionView.setText(restaurant.getDescription());
     }
 
 }

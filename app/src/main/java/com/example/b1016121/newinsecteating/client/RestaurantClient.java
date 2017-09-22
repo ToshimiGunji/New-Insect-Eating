617e867934a20bfa6180455cebe1968ccb1139fd
Keita <bonjovi1926@yahoo.co.jp>
2017-09-23 05:36:09 +0900

    add: api listview

---
+++ app/src/main/java/com/example/b1016121/newinsecteating/client/RestaurantClient.java
@@ -0,0 +1,18 @@
+package com.example.b1016121.newinsecteating.client;
+
+/**
+ * Created by keita on 2017/09/23.
+ */
+
+
+import retrofit2.http.GET;
+import retrofit2.http.QueryMap;
+import rx.Observable;
+import java.util.List;
+import java.util.Map;
+import com.example.b1016121.newinsecteating.model.Restaurant;
+
+public interface RestaurantClient {
+    @GET("/api/")
+    Observable<List<Restaurant>> search(@QueryMap Map<String,String> queryMap);
+}
---
+++ app/src/main/java/com/example/b1016121/newinsecteating/model/Restaurant.java
@@ -0,0 +1,79 @@
+package com.example.b1016121.newinsecteating.model;
+
+/**
+ * Created by keita on 2017/09/22.
+ */
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+
+public class Restaurant implements Parcelable {
+
+    private String name;
+    private String description;
+    private String address;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Parcelable.Creator<Restaurant> CREATOR
+            = new Parcelable.Creator<Restaurant>() {
+        @Override
+        public Restaurant createFromParcel(Parcel source) {
+            return new Restaurant(source);
+        }
+
+        @Override
+        public Restaurant[] newArray(int size) {
+            return new Restaurant[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(name);
+        out.writeString(description);
+        out.writeString(address);
+    }
+
+    public Restaurant(String name,String description, String address){
+        this.name = name;
+        this.description = description;
+        this.address = address;
+    }
+
+    private Restaurant(Parcel in){
+        name = in.readString();
+        description = in.readString();
+        address = in.readString();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+}

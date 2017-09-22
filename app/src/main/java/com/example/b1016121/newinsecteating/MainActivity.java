4cac436c009a95310be6f30a1fd3206010e987c8
Toshimi <toshimi0218@icloud.com>
2017-09-22 21:30:03 +0900

    nothing

---
+++ app/src/main/java/com/example/b1016121/newinsecteating/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.b1016121.newinsecteating;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

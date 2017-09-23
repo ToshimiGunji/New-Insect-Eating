2c0b79c55eaab82939aa6e185f4ecab54edddb86
Keita <bonjovi1926@yahoo.co.jp>
2017-09-23 09:39:34 +0900

    add: search function

--- app/src/main/java/com/example/b1016121/newinsecteating/client/RestaurantClient.java
+++ app/src/main/java/com/example/b1016121/newinsecteating/client/RestaurantClient.java
@@ -7,6 +7,7 @@ package com.example.b1016121.newinsecteating.client;
 
 import retrofit2.http.GET;
 import retrofit2.http.QueryMap;
+import retrofit2.http.Query;
 import rx.Observable;
 import java.util.List;
 import java.util.Map;
@@ -15,4 +16,7 @@ import com.example.b1016121.newinsecteating.model.Restaurant;
 public interface RestaurantClient {
     @GET("/api/")
     Observable<List<Restaurant>> search(@QueryMap Map<String,String> queryMap);
+
+    @GET("/api/search/")
+    Observable<List<Restaurant>> searchQue(@Query("query") String query);
 }
